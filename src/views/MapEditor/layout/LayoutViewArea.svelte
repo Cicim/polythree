16ff8477bdd6bd8331a8f1d59dc057dea00c776d
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2023-07-16 00:51:36 +0200

    MapEditor canvas auto-resizing

--- src/views/MapEditor/layout/LayoutViewArea.svelte
+++ src/views/MapEditor/layout/LayoutViewArea.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import MapEditor from "../editor/MapEditor.svelte";
 
     onMount(() => {
         console.log("Mounting");
     });
 </script>
+
+<div class="container">
+    <MapEditor />
+</div>
+
+<style lang="scss">
+    .container {
+        display: grid;
+        height: 100%;
+    }
+</style>
